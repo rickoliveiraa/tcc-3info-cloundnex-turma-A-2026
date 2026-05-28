49c737149e5f107f683a6691a3ef2e24eb5ef63e
Valentina de Oliveira Mendes <valentinaoliveira918@gmail.com>
2026-05-28 09:12:35 -0300

    Salvamento feito

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,25 @@
+/**
+ * 1. fazer download da biblioteca: npm install express
+ *      cria um servidor de páginas web/ -> nos criaremos uma api
+ * 2. importar as funções da biblioteca do meu projeto
+ *      linha 8 (import express .....)
+ * 3. instanciar as bibliotecas/funções (jogar para memória e executar)
+ * 
+ * 4. colocar o servidor web para escutar na porta específica
+ */
+
 import express from 'express';
 
 //Instanciando o servidor express()
 const server = express();
+
+const users = [
+    { id:1, name:'João da Silva', gender:'M', email:'joão.silva@email.com' },
+    { id:2, name:'Maria Mariana', gender:'F', email:'maria.mariana@email.com' },
+    { id:3, name:'Claudia Goes', gender:'F', email:'claudia.goes@email.com' },
+    { id:4, name:'Cristiano Ronaldo', gender:'M', email:'cristiano.ronaldo@email.com' },
+    { id:5, name:'Eddaughter Oliveira', gender:'F', email:'eddaughter.oliveira@email.com' },
+];
 
 /**
  * 1o. Parâmetro - Rota (endpoint)
@@ -15,6 +33,28 @@ server.get("/", (req, res)=>{
     res.send("Rota Raiz " + Date());
 });
 
+//Rota para acessar um usuário específico por ID
+server.get ("/users/:id", (req, res) => {
+    //Obtendo o id da rota
+    const id = parseInt(reportError.params.id);
+    //Buscando o usuário com o id correspondente
+    const user = users.find(u => u.id === id);
+
+    return res.status(200).json({
+        error: false,
+        message: 'User found',
+        result: user,
+    });
+});
+
+server.get('/users',(req, res)=>{
+    return res.status(200).json({
+        error: false,
+        message: 'Users list',
+        result: users,
+    });
+});
+
 //Iniciando o servidor na porta 3000
 /**
  * listen() - Método para iniciar o servidor
@@ -22,8 +62,8 @@ server.get("/", (req, res)=>{
  * 1o. Parâmetro - Porta (número da porta onde o servidor irá escutar as requisições)
  * 2o. Parâmetro - Callback (função a ser executada quando o servidor for iniciado)
  */
-server.listen(3000, ()=>{
-    console.log("Server on port 3000");
+server.listen(3333, ()=>{
+    console.log("Server on port 3333");
 });
 
 
